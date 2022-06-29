23ef05a996f85f367800b154b146a44704be8f85
Mathieu <mathieu.busse@live.fr>
2022-06-29 21:11:00 +0200

    code

--- 03. Générateur nombres de la suite de fibonacci/index.py
+++ 03. Générateur nombres de la suite de fibonacci/index.py
@@ -1,3 +1,12 @@
-#Générateur
+nombres = int(input("Entrez la quantité de nombres à afficher : "))
 
-print("Générateur")
+a = 0
+b = 1
+total = 0
+
+print("Les {0} premiers nombres de la suite de Fibonacci sont :".format(nombres), end=" ")
+for _ in range(nombres):
+    print(total, end=" ")
+    a = b
+    b = total
+    total = a + b
